9e00bfa0b175c36e40e2de215f8e17c97a70e593
Jack0ForREAL <129849911+Jack0ForREAL@users.noreply.github.com>
2025-11-27 02:23:25 +0300

    Update OptionsMenu.cs

--- Quaver.Shared/Screens/Options/OptionsMenu.cs
+++ Quaver.Shared/Screens/Options/OptionsMenu.cs
@@ -185,10 +185,9 @@ namespace Quaver.Shared.Screens.Options
                         new OptionsItemCheckbox(containerRect, "Enable High Quality", ConfigManager.VideoModHighQuality),
                         new OptionsItemCheckbox(containerRect, "Auto-Configure Performance", ConfigManager.VideoModAutoConfiguration),
                         
-                        new OptionsSlider(containerRect, "Max Resolution (Height)", ConfigManager.VideoModTargetHeight, i => i == 0 ? "Native" : $"{i}p")
+                       new OptionsSlider(containerRect, "Max Resolution (Height)", ConfigManager.VideoModTargetHeight, i => i == 0 ? "Native" : $"{i}p")
                         {
-                            Tags = new List<string> {"quality", "480p", "720p", "1080p"},
-                            Tooltip = "0 = Native. 480p is recommended for max FPS."
+                            Tags = new List<string> {"quality", "480p", "720p", "1080p"}
                         },
                         new OptionsSlider(containerRect, "Max RAM Budget", ConfigManager.VideoModRamBudget, i => $"{i}M")
                         {
