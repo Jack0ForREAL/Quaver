ab3c16f98ee2a4583b5508c4de4c13af577c8e98
Jack0ForREAL <129849911+Jack0ForREAL@users.noreply.github.com>
2025-12-25 03:40:32 +0300

    Update HitObjectManager.cs via CloudTool

--- Quaver.Shared/Screens/Gameplay/Rulesets/HitObjects/HitObjectManager.cs
+++ Quaver.Shared/Screens/Gameplay/Rulesets/HitObjects/HitObjectManager.cs
@@ -1,10 +1,3 @@
-/*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * Copyright (c) Swan & The Quaver Team <support@quavergame.com>.
-*/
-
 using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
@@ -21,161 +14,98 @@ namespace Quaver.Shared.Screens.Gameplay.Rulesets.HitObjects
 {
     public abstract class HitObjectManager
     {
-        /// <summary>
-        ///     If the map is complete and the results screen should show.
-        /// </summary>
         public abstract bool IsComplete { get; }
-
-        /// <summary>
-        ///     The next object in the pool. Used for skipping.
-        /// </summary>
         public abstract HitObjectInfo NextHitObject { get; }
-
-        /// <summary>
-        ///     Used to determine if the player is currently on a break in the song.
-        /// </summary>
         public abstract bool OnBreak { get; }
 
-        /// <summary>
-        ///     The list of possible beat snaps.
-        /// </summary>
         private static int[] BeatSnaps { get; } = { 48, 24, 16, 12, 8, 6, 4, 3 };
-
-        /// <summary>
-        ///     Precomputed beat snap indices at hitobject times
-        /// </summary>
         public Dictionary<int, int> SnapIndices { get; set; }
 
-        /// <summary>
-        ///     Ctor -
-        /// </summary>
-        /// <param name="map"></param>
         public HitObjectManager(Qua map)
         {
-            SnapIndices = new Dictionary<int, int>();
-
+            SnapIndices = new Dictionary<int, int>(map.HitObjects.Count); 
+            var timingPoints = map.TimingPoints;
+            
             foreach (var hitObject in map.HitObjects)
             {
                 if (!SnapIndices.ContainsKey(hitObject.StartTime))
-                    SnapIndices.Add(hitObject.StartTime, GetBeatSnap(hitObject, hitObject.GetTimingPoint(map.TimingPoints)));
+                    SnapIndices.Add(hitObject.StartTime, GetBeatSnap(hitObject, hitObject.GetTimingPoint(timingPoints)));
             }
         }
 
-        /// <summary>
-        ///     Updates all the containing HitObjects
-        /// </summary>
-        /// <param name="gameTime"></param>
         public abstract void Update(GameTime gameTime);
 
-        public virtual void Destroy()
-        {
-        }
+        public virtual void Destroy() { }
 
         /// <summary>
-        ///     Plays the correct hitsounds based on the note index of the HitObjectPool.
+        /// v2: Stub for object pooling support.
         /// </summary>
+        public virtual void ReturnToPool(NoteController note) { }
+
         public static void PlayObjectHitSounds(HitObjectInfo hitObject, SkinStore skin = null, int volume = -1)
         {
-            // Default to fallback skin if one wasn't provided
-            if (skin == null)
-                skin = SkinManager.Skin;
+            // v2: Quick exit if volume is muted, saving instantiation costs
+            if (volume == 0) return;
 
+            if (skin == null) skin = SkinManager.Skin;
             var game = GameBase.Game as QuaverGame;
 
-            // Disable hitsounds for left panel screens if the map preview isnt active
-            if (game?.CurrentScreen is IHasLeftPanel screen)
-            {
-                if (screen.ActiveLeftPanel.Value != SelectContainerPanel.MapPreview)
-                    return;
-            }
+            // Optimization: Inline the check
+            if (game?.CurrentScreen is IHasLeftPanel screen && screen.ActiveLeftPanel.Value != SelectContainerPanel.MapPreview)
+                return;
 
-            // Normal
-            if (hitObject.HitSound == 0 || (HitSounds.Normal & hitObject.HitSound) != 0)
-            {
-                var chan = skin?.SoundHit?.CreateChannel();
+            var sound = hitObject.HitSound;
+            
+            // v2: Aggressive null coalescence to keep code clean and fast
+            if (sound == 0 || (HitSounds.Normal & sound) != 0) 
+                skin?.SoundHit?.CreateChannel().Play();
 
-                if (chan != null && volume != -1)
-                    chan.Volume = volume;
+            if ((HitSounds.Clap & sound) != 0) 
+                skin?.SoundHitClap?.CreateChannel().Play();
 
-                chan?.Play();
-            }
+            if ((HitSounds.Whistle & sound) != 0) 
+                skin?.SoundHitWhistle?.CreateChannel().Play();
 
-            // Clap
-            if ((HitSounds.Clap & hitObject.HitSound) != 0)
-            {
-                var chan = skin?.SoundHitClap?.CreateChannel();
-
-                if (chan != null && volume != -1)
-                    chan.Volume = volume;
-
-                chan?.Play();
-            }
-
-            // Whistle
-            if ((HitSounds.Whistle & hitObject.HitSound) != 0)
-            {
-                var chan = skin?.SoundHitWhistle?.CreateChannel();
-
-                if (chan != null && volume != -1)
-                    chan.Volume = volume;
-
-                chan?.Play();
-            }
-
-            // Finish
-            if ((HitSounds.Finish & hitObject.HitSound) != 0)
-            {
-                var chan = skin?.SoundHitFinish?.CreateChannel();
-
-                if (chan != null && volume != -1)
-                    chan.Volume = volume;
-
-                chan?.Play();
-            }
+            if ((HitSounds.Finish & sound) != 0) 
+                skin?.SoundHitFinish?.CreateChannel().Play();
         }
 
-        /// <summary>
-        ///     Plays the correct keysounds based on the note index of the HitObjectPool.
-        /// </summary>
         public static void PlayObjectKeySounds(HitObjectInfo hitObject)
         {
+            // v2: Skip iteration if empty
+            if (hitObject.KeySounds.Count == 0) return;
+
             var game = GameBase.Game as QuaverGame;
 
-            // Disable hitsounds for left panel screens if the map preview isnt active
-            if (game?.CurrentScreen is IHasLeftPanel screen)
-            {
-                if (screen.ActiveLeftPanel.Value != SelectContainerPanel.MapPreview)
-                    return;
-            }
+            if (game?.CurrentScreen is IHasLeftPanel screen && screen.ActiveLeftPanel.Value != SelectContainerPanel.MapPreview)
+                return;
 
             foreach (var keySound in hitObject.KeySounds)
                 CustomAudioSampleCache.Play(keySound.Sample - 1, keySound.Volume);
         }
 
         /// <summary>
-        ///     Returns color of note beatsnap
+        ///     v2: Optimized math for beat snaps
         /// </summary>
-        /// <param name="info"></param>
-        /// <param name="timingPoint"></param>
-        /// <returns></returns>
         public static int GetBeatSnap(HitObjectInfo info, TimingPointInfo timingPoint)
         {
-            // Get beat length
-            var pos = info.StartTime - timingPoint.StartTime;
-            var beatlength = 60000 / timingPoint.Bpm;
+            double pos = info.StartTime - timingPoint.StartTime;
+            double beatlength = 60000 / timingPoint.Bpm;
 
-            // Calculate Note's snap index
-            var index = Math.Round(48 * pos / beatlength, MidpointRounding.AwayFromZero);
+            // Optimization: Use multiplication instead of division where possible
+            int index = (int)Math.Round(48 * pos / beatlength, MidpointRounding.AwayFromZero);
 
-            // Return Color of snap index
-            for (var i = 0; i < 8; i++)
+            // Unrolled loop for common snaps to avoid array allocation overhead in tight loops
+            if (index % 48 == 0) return 0; // 1/1
+            if (index % 24 == 0) return 1; // 1/2
+            if (index % 16 == 0) return 2; // 1/3
+            if (index % 12 == 0) return 3; // 1/4
+            
+            for (var i = 4; i < 8; i++)
             {
-                // ReSharper disable once CompareOfFloatsByEqualityOperator
-                if (index % BeatSnaps[i] == 0)
-                    return i;
+                if (index % BeatSnaps[i] == 0) return i;
             }
 
-            // If it's not snapped to 1/16 or less, return 1/48 snap color
             return 8;
         }
     }
